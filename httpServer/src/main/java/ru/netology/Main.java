afd19a1b716d4760d7c7f11fc681ca15d82e5f3c
Vasily Parshentsev <vparshentsev@gmail.com>
2023-05-31 17:15:48 +0300

    add handlers

--- httpServer/src/main/java/ru/netology/Main.java
+++ httpServer/src/main/java/ru/netology/Main.java
@@ -1,8 +1,30 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(9995);
-        server.start();
+        Server server = new Server();
+
+        // добавление хендлеров (обработчиков)
+        server.addHandler("GET", "/messages", (request, responseStream) -> {
+            try {
+                server.responseWithoutContent(responseStream, "404", "Not Found Dude");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                try {
+                    server.responseWithoutContent(responseStream, "404", "Not Found");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        server.start(9999);
     }
 }
+
